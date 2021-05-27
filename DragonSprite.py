ef41a0a6827e64d1ee5235966dd55547d5561077
TimmyNon <carlingtontimothy@yahoo.com>
2021-05-27 13:05:46 -0400

    harder dragon

--- DragonSprite.py
+++ DragonSprite.py
@@ -14,11 +14,11 @@ class DragonSprite(pygame.sprite.Sprite):
         self.rect.centerx = SCREENW / 2
         self.rect.centery = self.PH / 2
         self.mask = pygame.mask.from_surface(self.image)
-        self.health = 40
+        self.health = 60
         self.spawn = False
         self.bulletList = []
         self.timer = 0
-        self.timermax = 250
+        self.timermax = 100
         self.direction = "right"
         self.isTearSprite = False
 
